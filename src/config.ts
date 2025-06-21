a85b7385e2daecaade18a5244ebe27e48d105606
Damien Pontifex <damien.pontifex@gmail.com>
2025-06-22 07:35:06 +0800

    npm run bundle

--- src/config.ts
+++ src/config.ts
@@ -1,46 +1,48 @@
-import * as core from "@actions/core";
+import * as core from '@actions/core'
 
 export type ActionConfig = {
-  subscriptionId: string;
-  resourceGroupName: string;
-  containerAppName: string | undefined;
-  checkInterval: number;
-};
+  subscriptionId: string
+  resourceGroupName: string
+  containerAppName: string | undefined
+  checkInterval: number
+}
 
 function getInput(
   inputName: string,
   allowedValues?: string[],
-  throwOnMissing = true,
+  throwOnMissing = true
 ): string | undefined {
-  const inputValue = core.getInput(inputName)?.trim();
+  const inputValue = core.getInput(inputName)?.trim()
   if (!inputValue) {
     if (throwOnMissing) {
       throw new Error(
-        `Action input '${inputName}' is required but not provided`,
-      );
+        `Action input '${inputName}' is required but not provided`
+      )
     } else {
-      return;
+      return
     }
   }
 
   if (allowedValues && !allowedValues.includes(inputValue)) {
     throw new Error(
-      `Action input '${inputName}' must be one of the following values: '${allowedValues.join(`', '`)}'`,
-    );
+      `Action input '${inputName}' must be one of the following values: '${allowedValues.join(`', '`)}'`
+    )
   }
 
-  return inputValue;
+  return inputValue
 }
 export function parseConfig(): ActionConfig {
-  const subscriptionId = getInput("subscription-id") as string;
-  const resourceGroupName = getInput("resource-group-name") as string;
-  const containerAppName = getInput("container-app-name", undefined, false);
-  const checkInterval = parseInt(getInput("check-interval", undefined, false) as string);
+  const subscriptionId = getInput('subscription-id') as string
+  const resourceGroupName = getInput('resource-group-name') as string
+  const containerAppName = getInput('container-app-name', undefined, false)
+  const checkInterval = parseInt(
+    getInput('check-interval', undefined, false) as string
+  )
 
   return {
     subscriptionId,
     resourceGroupName,
     containerAppName,
-    checkInterval,
-  };
+    checkInterval
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,55 +1,75 @@
 import * as core from '@actions/core'
-import { DefaultAzureCredential } from '@azure/identity';
-import { ContainerAppsAPIClient } from '@azure/arm-appcontainers';
-import { ActionConfig, parseConfig } from './config.js';
+import { DefaultAzureCredential } from '@azure/identity'
+import { ContainerAppsAPIClient } from '@azure/arm-appcontainers'
+import { ActionConfig, parseConfig } from './config.js'
 
-const userAgentPrefix = "gh-damienpontifex-wait-for-container-app";
+const userAgentPrefix = 'gh-damienpontifex-wait-for-container-app'
 
-function createContainerAppsClient(config: ActionConfig): ContainerAppsAPIClient {
-  const credentials = new DefaultAzureCredential();
+function createContainerAppsClient(
+  config: ActionConfig
+): ContainerAppsAPIClient {
+  const credentials = new DefaultAzureCredential()
 
   return new ContainerAppsAPIClient(credentials, config.subscriptionId, {
     userAgentOptions: {
-      userAgentPrefix: userAgentPrefix,
-    },
-  });
+      userAgentPrefix: userAgentPrefix
+    }
+  })
 }
 
-async function containerAppsToWaitFor(config: ActionConfig, client: ContainerAppsAPIClient): Promise<string[]> {
+async function containerAppsToWaitFor(
+  config: ActionConfig,
+  client: ContainerAppsAPIClient
+): Promise<string[]> {
   if (!!config.containerAppName) {
-    core.debug(`Waiting for container app ${config.containerAppName} in resource group ${config.resourceGroupName}`);
-    return [config.containerAppName];
+    core.debug(
+      `Waiting for container app ${config.containerAppName} in resource group ${config.resourceGroupName}`
+    )
+    return [config.containerAppName]
   }
 
-  const resArray = new Array();
-  const apps = client.containerApps.listByResourceGroup(config.resourceGroupName);
+  const resArray = new Array()
+  const apps = client.containerApps.listByResourceGroup(
+    config.resourceGroupName
+  )
   for await (const app of apps) {
-    resArray.push(app.name);
+    resArray.push(app.name)
   }
-  core.debug(`Waiting for container apps in resource group ${config.resourceGroupName}: ${resArray.join(', ')}`);
-  return resArray;
+  core.debug(
+    `Waiting for container apps in resource group ${config.resourceGroupName}: ${resArray.join(', ')}`
+  )
+  return resArray
 }
 
-type AppsToRevisions = { [key: string]: string[] };
-async function revisionsToWaitFor(config: ActionConfig, apps: string[], client: ContainerAppsAPIClient): Promise<AppsToRevisions> {
-  const appsToRevisions: AppsToRevisions = {};
+type AppsToRevisions = { [key: string]: string[] }
+async function revisionsToWaitFor(
+  config: ActionConfig,
+  apps: string[],
+  client: ContainerAppsAPIClient
+): Promise<AppsToRevisions> {
+  const appsToRevisions: AppsToRevisions = {}
   for (const app of apps) {
-    const revisions = client.containerAppsRevisions.listRevisions(config.resourceGroupName, app);
+    const revisions = client.containerAppsRevisions.listRevisions(
+      config.resourceGroupName,
+      app
+    )
     for await (const revision of revisions) {
       if (revision.runningState !== 'Activating') {
-        continue;
+        continue
       }
 
       if (!appsToRevisions[app]) {
-        appsToRevisions[app] = [];
+        appsToRevisions[app] = []
       }
-      appsToRevisions[app].push(revision.name || '');
+      appsToRevisions[app].push(revision.name || '')
     }
     if (!appsToRevisions[app]) {
-      core.info(`No activating revisions needing to be waited for were found for container app ${app} in resource group ${config.resourceGroupName}`);
+      core.info(
+        `No activating revisions needing to be waited for were found for container app ${app} in resource group ${config.resourceGroupName}`
+      )
     }
   }
-  return appsToRevisions;
+  return appsToRevisions
 }
 
 /**
@@ -59,10 +79,10 @@ async function revisionsToWaitFor(config: ActionConfig, apps: string[], client: 
  */
 export async function run(): Promise<void> {
   try {
-    const config = parseConfig();
-    const client = createContainerAppsClient(config);
-    const apps = await containerAppsToWaitFor(config, client);
-    const appRevisions = await revisionsToWaitFor(config, apps, client);
+    const config = parseConfig()
+    const client = createContainerAppsClient(config)
+    const apps = await containerAppsToWaitFor(config, client)
+    const appRevisions = await revisionsToWaitFor(config, apps, client)
 
     // Iterate through the revisions and wait for them to finish activating
     // If a revision is still activating, log the time it has been activating
@@ -70,23 +90,37 @@ export async function run(): Promise<void> {
     while (Object.keys(appRevisions).length > 0) {
       for (const [appName, revisions] of Object.entries(appRevisions)) {
         for (const revision of revisions) {
-          const rev = await client.containerAppsRevisions.getRevision(config.resourceGroupName, appName, revision);
+          const rev = await client.containerAppsRevisions.getRevision(
+            config.resourceGroupName,
+            appName,
+            revision
+          )
           if (rev.runningState === 'Activating') {
-            const activatingForDuration = Math.floor((new Date().getTime() - rev.createdTime!.getTime()) / 1000);
-            core.info(`Revision ${revision} of container app ${appName} in resource group ${config.resourceGroupName} still activating for ${activatingForDuration} seconds`);
+            const activatingForDuration = Math.floor(
+              (new Date().getTime() - rev.createdTime!.getTime()) / 1000
+            )
+            core.info(
+              `Revision ${revision} of container app ${appName} in resource group ${config.resourceGroupName} still activating for ${activatingForDuration} seconds`
+            )
           } else {
-            core.info(`Revision ${revision} of container app ${appName} in resource group ${config.resourceGroupName} is in state ${rev.runningState}`);
-            delete appRevisions[appName]; // Remove from the list if it's not activating
+            core.info(
+              `Revision ${revision} of container app ${appName} in resource group ${config.resourceGroupName} is in state ${rev.runningState}`
+            )
+            delete appRevisions[appName] // Remove from the list if it's not activating
           }
         }
       }
 
       if (Object.keys(appRevisions).length > 0) {
-        await new Promise(resolve => setTimeout(resolve, 1000 * config.checkInterval)); // Wait before checking again
+        await new Promise((resolve) =>
+          setTimeout(resolve, 1000 * config.checkInterval)
+        ) // Wait before checking again
       }
     }
 
-    core.debug(`All container apps in resource group ${config.resourceGroupName} are no longer activating`);
+    core.debug(
+      `All container apps in resource group ${config.resourceGroupName} are no longer activating`
+    )
 
     // Set outputs for other workflow steps to use
     core.setOutput('time', new Date().toTimeString())
